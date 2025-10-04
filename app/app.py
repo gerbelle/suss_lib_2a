3449591d4bc599de2cff3182fc05b63d90d56397
ducklordGL <2104294J@student.tp.edu.sg>
2025-10-04 21:04:25 +0800

    hamburger working and everything else

--- app/app.py
+++ app/app.py
@@ -22,13 +22,13 @@ def get_book_by_title(book_title):
 @app.route('/')
 @app.route('/book_titles')
 def book_titles():
-    # 1. Get the selected category from the request
+    # 1. get the selected category from the request
     selected_category = request.args.get('category', 'All')
 
-    # 2. Start with all books
+    # 2. start with all books
     books_to_display = all_books
 
-    # 3. Filter the books if a specific category is selected
+    # 3. filter the books if a specific category is selected
     if selected_category != 'All':
         books_to_display = [
             book for book in all_books 
@@ -36,8 +36,8 @@ def book_titles():
         ]
 
    
-    sorted_books = sorted(books_to_display, key=lambda b: b['title'].lower())
-    unique_categories = get_unique_categories(all_books) 
+    sorted_books = sorted(books_to_display, key=lambda b: b['title'].lower())# sort alphabetically by title
+    unique_categories = get_unique_categories(all_books) # get unique categories for the dropdown
 
     return render_template(
         'book_titles.html', 
@@ -50,12 +50,12 @@ def book_titles():
     # sorted_books = sorted(all_books, key=lambda b: b['title'].lower())
     # return render_template('book_titles.html', books=sorted_books)
 
-@app.route('/book/<string:book_title>')
+@app.route('/book/<string:book_title>') #book detail page
 def book_detail(book_title):
     book = get_book_by_title(book_title)
 
     if book is None:
         return "Book not found", 404
 
-    # 3. Render the template
-    return render_template('book_detail.html', book=book)
+    
+    return render_template('book_detail.html', book=book) #render the book detail template with the book data
